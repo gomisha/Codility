858dc07644a2a8a10d74f9d561a4c16fca27136e
gomisha <misha@gomisha.com>
2018-05-21 16:05:46 -0400

    solution and tests for array rotation problem (Lesson 2): https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/

---
+++ src/com/codility/lesson2/cyclicrotation/Solution.java
@@ -0,0 +1,16 @@
+package com.codility.lesson2.cyclicrotation;
+
+//https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+public class Solution {
+	public int[] solution(int[] A, int K) {
+		int [] rotatedA = new int[A.length];
+		
+		for(int i=0; i<A.length; i++) {
+			//rotated index needs to "wrap" around end of array
+			int rotatedIndex = (i + K) % A.length;
+
+			rotatedA[rotatedIndex] = A[i];
+		}
+		return rotatedA;
+	}
+}
---
+++ src/test/com/codility/lesson2/cyclicrotation/SolutionTests.java
@@ -0,0 +1,47 @@
+package test.com.codility.lesson2.cyclicrotation;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import com.codility.lesson2.cyclicrotation.Solution;
+
+
+//https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+public class SolutionTests {
+	private Solution solution;
+	
+	@BeforeTest
+	public void setUp() {
+		solution = new Solution();
+	}
+	
+	@DataProvider(name = "test1")
+	public Object [][] createData1() {
+		return new Object [][] {
+			new Object [] { new int [] { 3, 8, 9,  7, 6 },  3,  new int [] {  9, 7, 6,  3, 8 } },
+			new Object [] { new int [] {       0,  0, 0 },  1,  new int [] {        0,  0, 0 } },
+			new Object [] { new int [] {    1, 2,  3, 4 },  4,  new int [] {     1, 2,  3, 4 } },
+			
+			//wrap around more than once
+			new Object [] { new int [] {    1, 2,  3, 4 },  5,  new int [] {     4, 1,  2, 3 } },
+			
+			//wrap around more than once, with negative
+			new Object [] { new int [] {   -1, 2, -3, 4 }, 10,  new int [] {    -3, 4, -1, 2 } },
+
+			//wrap around > 10 times - same as rotate by 3 (99 mod 4 = 3)
+			new Object [] { new int [] {   -1, 2, -3, 4 }, 99,  new int [] {    2, -3, 4, -1 } },
+			
+			//wrap around > 10 times - finish where it started
+			new Object [] { new int [] {   -1, 2, -3, 4 }, 100,  new int [] {   -1, 2, -3, 4 } },
+		};
+	}
+	
+	@Test(dataProvider = "test1")
+	public void veritySolution(int [] pA, int pK, int [] pExpectedRotatedA) {
+		int [] actualRotatedA = solution.solution(pA, pK);
+		Assert.assertEquals(pExpectedRotatedA.length, actualRotatedA.length);
+		for(int i=0; i<pExpectedRotatedA.length; i++) {
+			Assert.assertEquals(pExpectedRotatedA[i], actualRotatedA[i]);
+		}
+	}
+}
