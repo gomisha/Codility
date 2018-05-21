e02c59f8e101affba58f7cc5b7934a605e2eac62
gomisha <misha@gomisha.com>
2018-05-21 16:49:00 -0400

    solution and tests for odd occurrences in array problem (Lesson 2): https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/

---
+++ src/com/codility/lesson2/oddoccurrencesinarray/Solution.java
@@ -0,0 +1,32 @@
+package com.codility.lesson2.oddoccurrencesinarray;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class Solution {
+	public int solution(int[] A) {
+		HashMap <Integer, Integer> occurrencesMap = new HashMap<Integer, Integer>();
+		
+		for(int i=0; i<A.length; i++) {
+			if(occurrencesMap.containsKey(A[i])) {
+				int occurrences = occurrencesMap.get(A[i]);
+				occurrences++;
+				occurrencesMap.put(A[i], occurrences); //increment occurrence counter and store it
+			}
+			else {
+				occurrencesMap.put(A[i], 1); //1st occurrences of this value
+			}
+		}
+
+		Set<Integer> keySet = occurrencesMap.keySet();
+		
+		for(int currentKey : keySet) {
+			int occurrences = occurrencesMap.get(currentKey);
+			
+			//if occurs odd number of times, we found the unpaired value - no need to continue checking
+			if(occurrences % 2 != 0) return currentKey;
+		}
+		//should never get to here
+		throw new RuntimeException("shouldn't get to here - should've return unpaired value by now");
+	}
+}
---
+++ src/test/com/codility/lesson2/oddoccurrencesinarray/SolutionTests.java
@@ -0,0 +1,35 @@
+package test.com.codility.lesson2.oddoccurrencesinarray;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import com.codility.lesson2.oddoccurrencesinarray.Solution;
+
+
+public class SolutionTests {
+	private Solution solution;
+	
+	@BeforeTest
+	public void setUp() {
+		solution = new Solution();
+	}
+	
+	@DataProvider(name = "test1")
+	public Object [][] createData1() {
+		return new Object [][] {
+			new Object [] { new int [] { 9, 3, 9, 3, 9, 7, 9 },  7 },
+			new Object [] { new int [] { 1, 2, 1, 3, 5, 2, 3 },  5 },
+
+			//double pair matching
+			new Object [] { new int [] { 1, 2, 1, 3, 5, 2, 3, 1, 1, 2, 2 },  5 },
+			
+			//large numbers
+			new Object [] { new int [] { 1000000, 2000000, 1000000, 30000000, 5000000, 2000000, 30000000 },  5000000 },
+		};
+	}
+
+	@Test(dataProvider = "test1")
+	public void verifySolution(int [] pA, int pExpectedNonPair) {
+		Assert.assertEquals(solution.solution(pA), pExpectedNonPair);
+	}
+}
