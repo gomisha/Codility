e2230c311f986240b270e01fef2c17523c5a85ef
gomisha <misha@gomisha.com>
2018-05-21 14:04:26 -0400

    minor reformatting/var name change

--- src/test/com/codility/lesson1/binarygap/SolutionTests.java
+++ src/test/com/codility/lesson1/binarygap/SolutionTests.java
@@ -7,11 +7,11 @@ import com.codility.lesson1.binarygap.Solution;
 
 //https://app.codility.com/programmers/lessons/1-iterations/
 public class SolutionTests {
-	private Solution binaryGapSolution;
+	private Solution solution;
 	
 	@BeforeTest
 	public void setUp() {
-		binaryGapSolution = new Solution();
+		solution = new Solution();
 	}
 	
 	//http://acc6.its.brooklyn.cuny.edu/~gurwitz/core5/nav2tool.html
@@ -20,24 +20,24 @@ public class SolutionTests {
 	@DataProvider(name = "test1")
 	public Object [][] createData1() {
 		return new Object [][] {
-			new Object [] {    0, 0 },
-			new Object [] {    9, 2 },
-			new Object [] {   15, 0 },
-			new Object [] {   32, 0 },
-			new Object [] {  529, 4 },
-			new Object [] { 1041, 5 },
-			new Object [] { 65536, 0 },
-			new Object [] { 65537, 15 },
-			new Object [] { 100000, 4 }, 
-			new Object [] { 2147483, 5 },
-			new Object [] { 2147483637, 1 }, //max - 10
-			new Object [] { 2147483646, 0 }, //max - 1
-			new Object [] { 2147483647, 0 } //max
+			new Object [] {          0,  0 },
+			new Object [] {          9,  2 },
+			new Object [] {         15,  0 },
+			new Object [] {         32,  0 },
+			new Object [] {        529,  4 },
+			new Object [] {       1041,  5 },
+			new Object [] {      65536,  0 },
+			new Object [] {      65537, 15 },
+			new Object [] {     100000,  4 }, 
+			new Object [] {    2147483,  5 },
+			new Object [] { 2147483637,  1 }, //max - 10
+			new Object [] { 2147483646,  0 }, //max - 1
+			new Object [] { 2147483647,  0 }  //max
 		};
 	}
 	
 	@Test(dataProvider = "test1")
 	public void veritySolution(int pInput, int expectedBinaryGap) {
-		Assert.assertEquals(binaryGapSolution.solution(pInput), expectedBinaryGap);
+		Assert.assertEquals(solution.solution(pInput), expectedBinaryGap);
 	}
 }
