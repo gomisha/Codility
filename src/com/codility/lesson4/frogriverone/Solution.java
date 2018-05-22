4d72148133122903f92851014b703f6b39e4652c
gomisha <misha@gomisha.com>
2018-05-22 10:50:50 -0400

    solution and tests for Frog River One problem (Lesson 4): https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/

---
+++ src/com/codility/lesson4/frogriverone/Solution.java
@@ -0,0 +1,28 @@
+package com.codility.lesson4.frogriverone;
+
+import java.util.HashSet;
+import java.util.Set;
+
+//https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/
+
+public class Solution {
+	public int solution(int X, int[] A) {
+		Set<Integer>requiredLeavesSet = new HashSet<Integer>();
+		for(int i=1; i<=X; i++) {
+			requiredLeavesSet.add(i);
+		}
+		
+		Set<Integer>currentLeavesSet = new HashSet<Integer>();
+		
+		for(int p=0; p<A.length; p++) {
+			currentLeavesSet.add(A[p]);
+			//keep adding to current leaves set until it's at least the same size as required leaves set
+			if(currentLeavesSet.size() < requiredLeavesSet.size()) continue;
+			
+			if(currentLeavesSet.containsAll(requiredLeavesSet)) {
+				return p;
+			}
+		}
+		return -1;
+	}
+}
---
+++ src/test/com/codility/lesson4/frogriverone/SolutionTests.java
@@ -0,0 +1,36 @@
+package test.com.codility.lesson4.frogriverone;
+
+//https://app.codility.com/programmers/lessons/4-counting_elements/frog_river_one/
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import com.codility.lesson4.frogriverone.Solution;
+
+public class SolutionTests {
+	private Solution solution;
+	
+	@BeforeTest
+	public void setUp() {
+		solution = new Solution();
+	}
+
+	@DataProvider(name = "test1")
+	public Object [][] createData1() {
+		return new Object [][] {
+			new Object [] { 5, new int [] {             1, 3, 1, 4, 2, 3, 5, 4 },  6 },
+			new Object [] { 3, new int [] {                               1, 3 }, -1 }, //never gets across
+			new Object [] { 2, new int [] {                         1, 1, 1, 1 }, -1 }, //never gets across
+			new Object [] { 3, new int [] {                         1, 4, 2, 3 },  3 },
+			new Object [] { 2, new int [] {                         1, 4, 2, 3 },  2 },
+			new Object [] { 4, new int [] { 1, 2, 3, 2, 3, 3, 1, 2, 2, 4, 2, 1 },  9 },
+			new Object [] { 4, new int [] { 1, 2, 3, 2, 3, 3, 1, 2, 4, 4, 2, 1 },  8 },
+			new Object [] { 4, new int [] { 1, 2, 3, 4, 3, 3, 1, 2, 4, 4, 2, 1 },  3 },
+		};
+	}
+
+	@Test(dataProvider = "test1")
+	public void verifySolution(int pX, int [] pA, int pExpected) {
+		Assert.assertEquals(solution.solution(pX, pA), pExpected);
+	}
+}
