d5380352ce41a2adb2b72c412d8bef3428795802
gomisha <misha@gomisha.com>
2018-05-30 18:41:49 -0400

    uncommented a test

--- src/test/com/codility/lesson9/maxslice/SolutionTests.java
+++ src/test/com/codility/lesson9/maxslice/SolutionTests.java
@@ -18,7 +18,7 @@ public class SolutionTests {
 	@DataProvider(name = "test1")
 	public Object [][] createData1() {
 		return new Object [][] {
-			//new Object [] { new int [] { 5, -7, 3, 5, -2, 4, -1 }, 10 },
+			new Object [] { new int [] { 5, -7, 3, 5, -2, 4, -1 }, 10 },
 			new Object [] { new int [] { -2, -3, 4, -1, -2, 1, 5, -3 } , 7 },
 		};
 	}
