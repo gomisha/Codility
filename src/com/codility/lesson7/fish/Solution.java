204d876f7a55ff53e408d562240ec64bf4199c99
gomisha <misha@gomisha.com>
2018-05-29 20:27:13 -0400

    solution3 (refactoring) for Fish problem (Lesson 7): https://app.codility.com/programmers/lessons/7-stacks_and_queues/fish/ (100% correct, 100% performance: 100% overall)

--- src/com/codility/lesson7/fish/Solution.java
+++ src/com/codility/lesson7/fish/Solution.java
@@ -7,57 +7,42 @@ import java.util.Stack;
 public class Solution {
 	public int solution(int[] A, int[] B) {
 		final int UPSTREAM = 0;
-		final int DOWNSTREAM = 1;
 		
 		Stack<Fish> fishStack = new Stack<Fish>();
+		fishStack.push(new Fish(A[0], B[0]));
 
-		int aliveUpstreamFishCounter = 0;
-		for(int i=0; i<A.length; i++) {
-			if(fishStack.isEmpty() && B[i] == DOWNSTREAM) {
-				//don't count this fish yet - need to see if it will survive
+		for(int i=1; i<A.length; i++) {
+			if(B[i] == fishStack.peek().direction) { //same direction, so put another fish on stack
 				fishStack.push(new Fish(A[i], B[i]));
 			}
-			else if(fishStack.isEmpty() && B[i] == UPSTREAM) {
-				//this fish will survive because it's UPSTREAM and there's no DOWNSTREAM fish above it
-				aliveUpstreamFishCounter++;
+			//if top of stack fish is upstream, not right condition to see who's eating who
+			else if(fishStack.peek().direction == UPSTREAM) {
+				fishStack.push(new Fish(A[i], B[i]));
 			}
-			else {
-				Fish stackFish = fishStack.peek();
-				if(B[i] == stackFish.direction) { //same direction, so put another fish on stack
-					fishStack.push(new Fish(A[i], B[i]));
-				}
-				//if top of stack fish is upstream, not right condition to see who's eating who
-				else if(stackFish.direction == UPSTREAM) {
-					fishStack.push(new Fish(A[i], B[i]));
-				}
-				else { //figure out who's eating who
-					while(!fishStack.isEmpty()) {
-						Fish topOfStackFish = fishStack.peek();
-						if(topOfStackFish.direction != B[i]) {
-							//existing fish that was on stack eats latest fish
-							if(topOfStackFish.size > A[i]) {
-								//fishStack.push(topOfStackFish);
-								break; //eating finished
-							}
-							else {
-								fishStack.pop();
-								continue; //keep checking other elements on stack
-								//fishStack.push(new Fish(A[i], B[i]));
-							}
+			else { //figure out who's eating who
+				while(!fishStack.isEmpty()) {
+					//current fish is swimming in same direction as top of stack fish - they can't eat other
+					if(fishStack.peek().direction == B[i]) {
+						fishStack.push(new Fish(A[i], B[i]));
+						break;
+					}
+					else {
+						//existing fish that was on stack eats latest fish
+						if(fishStack.peek().size > A[i]) {
+							break; //eating finished
 						}
 						else {
-							fishStack.push(new Fish(A[i], B[i]));
-							break;
+							fishStack.pop();
+							continue; //keep checking other elements on stack
 						}
 					}
-					if(fishStack.isEmpty()) {
-						fishStack.push(new Fish(A[i], B[i])); //current fish ate all the fish in the stack
-					}
+				}
+				if(fishStack.isEmpty()) {
+					fishStack.push(new Fish(A[i], B[i])); //current fish ate all the fish in the stack
 				}
 			}
 		}
-		
-		return aliveUpstreamFishCounter + fishStack.size();
+		return fishStack.size();
 	}
 	
 	class Fish {
