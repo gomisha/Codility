37fccc668adbe00b9578dcdce624b716269e4874
gomisha <misha@gomisha.com>
2018-05-30 11:10:05 -0400

    solution and tests for Dominator problem (Lesson 8): https://app.codility.com/programmers/lessons/8-leader/dominator/ (100% correct, 100% performance: 100% overall)

---
+++ src/com/codility/lesson8/dominator/Solution.java
@@ -0,0 +1,56 @@
+package com.codility.lesson8.dominator;
+
+import java.util.Stack;
+
+//https://app.codility.com/programmers/lessons/8-leader/dominator/
+
+//main strategy: if there's a dominator (number occurs more than half the time), then it will be the only 
+//number on a stack to find candidates
+//keep a stack of duplicate candidates; if encounter a number that doesn't exist on top of stack,
+//pop the stack; if encounter number that equals the top of stack number, push that number on top of stack
+//if the stack is not empty after going through all the array elements, then we have a candidate (not necessarily a dominator)
+//go through array again counting number of occurences of candidate to decide if it's dominator
+//
+//reference: https://codility.com/media/train/6-Leader.pdf
+
+public class Solution {
+
+	public int solution(int[] A) {
+		Stack <Integer>stack = new Stack<Integer>();
+		
+		for(int i=0; i<A.length; i++) {
+			if(stack.isEmpty()) {
+				stack.push(A[i]);
+			}
+			else {
+				if(stack.peek().intValue() == A[i]) {
+					stack.push(A[i]);
+				}
+				else {
+					stack.pop();
+				}
+			}
+		}
+		//no candidate if stack is empty
+		if(stack.isEmpty()) return -1;
+		int candidate = stack.peek().intValue();
+		int count = 0;
+		int candidateIndex = -1;
+		for(int i=0; i<A.length; i++) {
+			if(A[i] == candidate) {
+				count++;
+				if(candidateIndex < 0) {
+					candidateIndex = i; //only store index of first occurence of candidate
+				}
+			}
+		}
+
+		//works for even and odd number of A elements
+		//e.g. if A.length = 4, count needs to be > 2
+		//e.g. if A.length = 5, count needs to be > 2
+		if(count > (A.length / 2)) return candidateIndex;
+
+		return -1; //no dominator found
+	}
+
+}
---
+++ src/test/com/codility/lesson8/dominator/SolutionTests.java
@@ -0,0 +1,37 @@
+package test.com.codility.lesson8.dominator;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.codility.lesson8.dominator.Solution;
+
+//https://app.codility.com/programmers/lessons/8-leader/dominator/
+
+public class SolutionTests {
+	private Solution solution;
+	
+	@BeforeTest
+	public void setUp() {
+		solution = new Solution();
+	}
+
+	@DataProvider(name = "test1")
+	public Object [][] createData1() {
+		return new Object [][] {
+			new Object [] { new int [] { 3, 4, 3, 2, 3, -1, 3, 3 }, 0 },
+			new Object [] { new int [] { 100, 100, 100, 100, 100, 100  }, 0 },
+			new Object [] { new int [] { 100, -1000, 100, -1000, -1000  }, 1 },
+			new Object [] { new int [] { 1, 2, 3  }, -1 },
+			new Object [] { new int [] { 1000000000  }, 0 },
+			new Object [] { new int [] { 1000, 1  }, -1 },
+			new Object [] { new int [] { 0, 0  }, 0 },
+		};
+	}
+
+	@Test(dataProvider = "test1")
+	public void verifySolution(int [] pA, int pExpected) {		
+		Assert.assertEquals(solution.solution(pA), pExpected);
+	}	
+}
