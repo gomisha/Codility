cf5e08baa8cecb585eee64fea6e8777e8bb1a098
gomisha <misha@gomisha.com>
2018-05-31 09:45:20 -0400

    solution and tests for Max Double Slice problem (Lesson 9): https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_double_slice_sum/ (100% correct, 100% performance: 100% overall)

---
+++ src/com/codility/lesson9/maxslice/MaxDoubleSlice.java
@@ -0,0 +1,38 @@
+package com.codility.lesson9.maxslice;
+
+//reference: https://stackoverflow.com/questions/25726735/maxdoubleslicesum-codility-algorithm
+//reference: https://rafal.io/posts/codility-max-double-slice-sum.html
+//https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_double_slice_sum/
+//strategy:
+//Compute local max (not less than 0) up to each element in A going a) forward and b) backward
+//find absolute max double slice by going through a) and b) to compute each double slice sum
+//
+//e.g.         A = {  3,  2,  6, -1,  4,  5, -1, 2 }
+//slice1LocalMax = {  0,  2,  8,  7, 11, 16, 15, 0 }
+//slice2LocalMax = {  0, 16, 14,  8,  9,  5,  0, 0 }
+
+public class MaxDoubleSlice {
+	public int solution(int[] A) {
+		int[] slice1LocalMax = new int[A.length];
+		int[] slice2LocalMax = new int[A.length];
+		
+		//start from i=1 because slice can't start at index 0
+		for(int i = 1; i < A.length-1; i++) {
+			slice1LocalMax[i] = Math.max(slice1LocalMax[i-1] + A[i], 0);
+		}
+		  
+		//start from i=A.length-2 because slice can't end at index A.length-1
+		for(int i = A.length-2; i > 0; i--){
+			slice2LocalMax[i] = Math.max(slice2LocalMax[i+1]+A[i], 0);
+		}
+		
+		int maxDoubleSliceSum = 0;
+		
+		//compute sums of all slices to find absolute max
+		for(int i = 1; i < A.length-1; i++) {
+			maxDoubleSliceSum = Math.max(maxDoubleSliceSum, slice1LocalMax[i-1] + slice2LocalMax[i+1]);
+		}
+		
+		return maxDoubleSliceSum;
+	}
+}
---
+++ src/test/com/codility/lesson9/maxslice/MaxDoubleSliceSumTests.java
@@ -0,0 +1,33 @@
+package test.com.codility.lesson9.maxslice;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.codility.lesson9.maxslice.MaxDoubleSlice;
+
+//https://app.codility.com/programmers/lessons/9-maximum_slice_problem/max_double_slice_sum/
+
+public class MaxDoubleSliceSumTests {
+	private MaxDoubleSlice solution;
+	
+	@BeforeTest
+	public void setUp() {
+		solution = new MaxDoubleSlice();
+	}
+
+	@DataProvider(name = "test1")
+	public Object [][] createData1() {
+		return new Object [][] {
+			new Object [] { new int [] { 3, 2, 6, -1, 4, 5, -1, 2 }, 17 },
+			new Object [] { new int [] { -2 -3, 4, -1, -2, 1, 5, -3 }, 9 },
+			new Object [] { new int [] { 1, 2, 3 }, 0 },
+		};
+	}
+
+	@Test(dataProvider = "test1")
+	public void verifySolution(int [] pA, int pExpected) {		
+		Assert.assertEquals(solution.solution(pA), pExpected);
+	}	
+}
